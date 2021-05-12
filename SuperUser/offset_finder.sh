cbb6afc6d591fa782af47ae6c08b6b49c5fa02ef
Lorenzo Santina <lorenzo.santina@gmail.com>
2021-05-12 14:06:17 +0200

    Offset finder elf offset

--- SuperUser/offset_finder.sh
+++ SuperUser/offset_finder.sh
@@ -1,7 +1,6 @@
-base_address=$(readelf -l $2 | grep LOAD | grep "R E" | awk "{print \$3}")
+runtime_base_address=$(readelf -l $2 | grep LOAD | grep "R E" | awk "{print \$3}")
+elf_offset=$(strings -a -t x $2 | grep $1 | awk '{print "0x"$1}')
+runtime_offset=$((runtime_base_address + elf_offset))
 
-string_elf_offset=$(strings -a -t x $2 | grep $1 | awk '{print "0x"$1}')
-
-string_offset=$((base_address + string_elf_offset))
-
-echo "OFFSET = $string_offset"
+echo "ELF OFFSET = $((elf_offset))"
+echo "Runtime OFFSET = $runtime_offset"
